f73d2f515063e25ccfd500c9629638c434063540
root <root@erickurquilla.localdomain>
2025-07-19 15:00:01 -0600

    Cambiando la posicion de la funcion que calcula los polinomios de lagrange a la bases.py

--- cdg_fuente/bases.py
+++ cdg_fuente/bases.py
@@ -22,13 +22,49 @@ def lagrange_basis_derivative(nodes, i, x):
             basis_derivative += pc/(nodes[i]-nodes[j])
     return basis_derivative
 
-def generate_reference_space(malla_, cuadratura_de_gauss_, polinomios_de_lagrange_):
+def polinomios_de_lagrange(nodos, i, x):
+    '''
+    Dado un conjunto de nodos en un elemento esta funcion
+    evalua el polinomio de Lagrange i (es decir el polinomio de
+    lagrange que es uno cuando es evaluado en la posicion del nodo i)
+    en x la posicion x
+
+    Parámetros:
+        nodos (numpy.ndarray): Array de nodos de un elemento de la malla.
+            ejemplo: nodos = np.array([x0, x1, ..., xN]) donde N es el numero de nodos por elemento
+        i (int): Índice del nodo en el cual el polinomio de lagrange es uno.
+            ejemplo: i = 1
+        x (float): Punto en el cual se evalúa la función base.
+            ejemplo: x = 0.5
+
+    Retorna:
+        float: Valor de la función base de Lagrange i en el punto x.
+    '''
+
+    # inicializa el polinomio de lagrange i evaluado en x
+    polinomio_de_lagrange_i_evaluado_en_x = 1.0
+
+    #-----------------------------------------------------------------------------------------
+    # Escribe tu solucion al ejercicio 3 a continuacion ...
+
+    # calcula el polinomio de lagrange i evaluado en x
+    for j in range(len(nodos)):
+        # si j es diferente de i
+        if j != i:
+            # multiplica el polinomio de lagrange i evaluado en x por el factor correspondiente
+            polinomio_de_lagrange_i_evaluado_en_x *= (x - nodos[j]) / (nodos[i] - nodos[j])
+
+    #-----------------------------------------------------------------------------------------
+
+    return polinomio_de_lagrange_i_evaluado_en_x
+
+def generate_reference_space(malla_, cuadratura_de_gauss_):
 
     # guardando la cuadratura de Gauss en el espacio físico
     cuadratura_de_gauss_espacio_fisico = 0.5 * ( malla_[0][-1] - malla_[0][0] ) * cuadratura_de_gauss_ + 0.5 * ( malla_[0][-1] + malla_[0][0])
 
     # evaluando la función base en los puntos de cuadratura de Gauss
-    polinomios_de_lagrange_en_cuadratura_de_gauss_ = np.array([[polinomios_de_lagrange_(malla_[0], i, x) for x in cuadratura_de_gauss_espacio_fisico] for i in range(len(malla_[0]))])
+    polinomios_de_lagrange_en_cuadratura_de_gauss_ = np.array([[polinomios_de_lagrange(malla_[0], i, x) for x in cuadratura_de_gauss_espacio_fisico] for i in range(len(malla_[0]))])
 
     # evaluando la derivada en x de la función base evaluada en los puntos de cuadratura de Gauss
     derivada_x_polinomios_de_lagrange_en_cuadratura_de_gauss_ = np.array([[lagrange_basis_derivative(malla_[0], i, x) for x in cuadratura_de_gauss_espacio_fisico] for i in range(len(malla_[0]))])
